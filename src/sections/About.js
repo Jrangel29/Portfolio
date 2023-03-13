cf09b465d9e8aebd7380a5afe22ece3e888cf0b6
Jrangel29 <Jrangel29@ua.pt>
2023-03-13 10:47:28 +0000

    About Section - Content Done - Minimal Styling

--- src/sections/About.js
+++ src/sections/About.js
@@ -8,56 +8,60 @@ const About = () => {
     return(
         <Box sx={{width: '100vw'}}>
             <Container>
-                <h1>About Me</h1>
-                <Grid container>
-                    <Grid item container xs={12} md={6}>
+                <h1 className='sectionTitle'>About Me</h1>
+                <Grid container spacing={3}>
+                    <Grid item container xs={12} md={6}  className='gridHeights'>
                         <Grid item xs={12}>
-                            <p>Who am I</p>
+                            <p className='sectionSubtitle'>Who am I</p>
                         </Grid>
-                        <Grid item xs={12}>
+                        <Grid item xs={12} md={6}>
                             <img src={profile} className='profilePicture'/>
                         </Grid>
-                        <Grid item xs={12}>
-                            <p>Hi</p>
+                        <Grid item xs={12} md={6}>
+                            <p className='explanationText'>
+                                Hi! I'm a web developer. Most of my experience is in front-end development, but I have worked with back-end in some projects.
+                                <br className='textBreak'/>
+                                I have a bachelor's degree in New Technologies of Communication from the University of Aveiro and I've recently finished my master's degree in Communication and Multimedia at the same university.
+                            </p>
                         </Grid>
                     </Grid>
-                    <Grid item container xs={12} md={6}>
+                    <Grid item container xs={12} md={6} className='gridHeights'>
                         <Grid item xs={12}>
-                            <p>Some of the main technologies I've worked with</p>
+                            <p className='sectionSubtitle'>Some of the main technologies I've worked with</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-html5-plain colored iconStyle"></i>
                             <p>HTML5</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-css3-plain colored iconStyle"></i>
                             <p>CSS3</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-bootstrap-plain colored iconStyle"></i>
                             <p>Bootstrap</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-javascript-plain colored iconStyle"></i>
                             <p>Javascript</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-react-original colored iconStyle"></i>
                             <p>React.JS</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-redux-original colored iconStyle"></i>
                             <p>Redux</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-express-original colored iconStyle"></i>
                             <p>Express.JS</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs={6} sm={4}>
                             <i class="devicon-firebase-plain colored iconStyle"></i>
                             <p>Firebase</p>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        <Grid item xs>
                             <i class="devicon-mysql-plain colored iconStyle"></i>
                             <p>MySQL</p>
                         </Grid>
